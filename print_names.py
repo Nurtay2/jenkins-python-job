6f53d8307c6f7653685bf7a2b388169e28b09597
Nurtay2 <122880800+Nurtay2@users.noreply.github.com>
2025-02-22 09:56:43 +0500

    Update print_names.py

--- print_names.py
+++ print_names.py
@@ -1,2 +1,8 @@
-print("Kurmangazy Nurzhan")
-print("Kurmangazy Nurzhan")
+import sys
+
+if len(sys.argv) > 1:
+    name = sys.argv[1]
+else:
+    name = "Nurzhan Kurmangazy"  
+
+print(f"My name is {name}")
